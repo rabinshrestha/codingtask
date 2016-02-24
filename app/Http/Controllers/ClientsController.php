a2b19c9027234403269075fa2f65f017b57846a3
rabin <shrestha.rabin505@gmail.com>
2016-02-24 08:02:18 +0545

    Form validation and csv library usage

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\Controller;
 use Request;
 use Illuminate\Support\Facades\Validator;
 
+use League\Csv\Reader;
+use League\Csv\Writer;
+
 class ClientsController extends Controller {
 
 	private static $validationRule = [
-        'fullName' 		=> 'required|max:50|alpha_num',
+        'fullName' 		=> 'required|min:5|max:50',
         'inputGender'	=> 'required',
-        'phone' 		=> 'numeric|max:10',
+        'phone' 		=> 'digits_between:0,10',
         'email' 		=> 'email|max:100',
         'address'		=> 'max:100',
         'nationality'	=> 'required|max:50',
@@ -20,6 +23,8 @@ class ClientsController extends Controller {
         'education'		=> 'max:100',
         'contactMode'	=> 'required',
     ];
+
+    private static $csvFileName = 'clients.csv';
 
 	/**
 	 * Display a listing of the resource.
@@ -51,10 +56,17 @@ class ClientsController extends Controller {
 		$input = Request::only('fullName', 'inputGender', 'phone', 'email', 'address', 'nationality', 'dateOfBirth', 'education', 'contactMode');
 		$validator = Validator::make($input, ClientsController::$validationRule);
 		if($validator->fails())
-			dd($validator->messages());
-		// dd($validator);
-		// $input = Request::all();
-		// dd($input);
+			return redirect('clients/create')
+						->withErrors($validator)
+						->withInput();
+
+		//store data
+		// $reader = Reader::createFromPath('/path/to/your/csv/file.csv');
+		//the $reader object will use the 'r+' open mode as no `open_mode` parameter was supplied.
+		$writer = Writer::createFromPath(new SplFileObject(storage_path . ClientsController::$csvFileName, 'a+'), 'w');
+		$writer->insertOne(['john', 'doe', 'john.doe@example.com']);
+		return "Data Inserrted";
+		//the $writer object open mode will be 'w'!!				
 	}
 
 	/**
--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -5,43 +5,53 @@
 			<div class="panel-heading">
 				Please insert client information.
 			</div>
+			@if (count($errors) > 0)
+			    <div class="alert alert-danger">
+			        <ul>
+			            @foreach ($errors->all() as $error)
+			                <li>{{ $error }}</li>
+			            @endforeach
+			        </ul>
+			    </div>
+			@endif
+
 			<div class="panel-body">
 			    {!! Form::open(array('url'=>'clients')) !!}
 			    	<div class="form-group">
-			            {!! Form::label('Name', '', array('for'=>'fullName')) !!}
-			            {!! Form::text('fullName', '', array('class'=>'form-control', 'placeholder'=>'Name', 'id'=>'fullName')) !!}
+			            {!! Form::label('Full Name', '', array('for'=>'fullName')) !!}
+			            {!! Form::text('fullName', Input::old('fullName'), array('class'=>'form-control', 'placeholder'=>'Full Name', 'id'=>'fullName')) !!}
 			        </div>
 			        <div class="form-group">
 			        	{!! Form::label('Gender', '', array('for'=>'inputGender')) !!}
-			        	{!! Form::select('inputGender', array('male'=>'Male','female'=>'Female','other'=>'Other'),'male',array('class'=>'form-select form-control')) !!}
+			        	{!! Form::select('inputGender', array('male'=>'Male','female'=>'Female','other'=>'Other'),Input::old('inputGender'),array('class'=>'form-select form-control')) !!}
 			        </div>
 			        <div class="form-group">
 			            {!! Form::label('Phone', '', array('for'=>'phone')) !!}
-			            {!! Form::text('phone', '', array('class'=>'form-control', 'placeholder'=>'Phone', 'id'=>'phone')) !!}
+			            {!! Form::text('phone', Input::old('phone'), array('class'=>'form-control', 'placeholder'=>'Phone', 'id'=>'phone')) !!}
 			        </div>
 			        <div class="form-group">
 			            {!! Form::label('Email', '', array('for'=>'email')) !!}
-			            {!! Form::email('email', '', array('class'=>'form-control', 'placeholder'=>'Email', 'id'=>'email')) !!}
+			            {!! Form::email('email', Input::old('email'), array('class'=>'form-control', 'placeholder'=>'Email', 'id'=>'email')) !!}
 			        </div>
 			        <div class="form-group">
 			            {!! Form::label('Address', '', array('for'=>'address')) !!}
-			            {!! Form::text('address', '', array('class'=>'form-control', 'placeholder'=>'Address', 'id'=>'address')) !!}
+			            {!! Form::text('address', Input::old('address'), array('class'=>'form-control', 'placeholder'=>'Address', 'id'=>'address')) !!}
 			        </div>
 			        <div class="form-group">
 			            {!! Form::label('Nationality', '', array('for'=>'nationality')) !!}
-			            {!! Form::text('nationality', '', array('class'=>'form-control', 'placeholder'=>'Nationality', 'id'=>'nationality')) !!}
+			            {!! Form::text('nationality', Input::old('nationality'), array('class'=>'form-control', 'placeholder'=>'Nationality', 'id'=>'nationality')) !!}
 			        </div>
 			        <div class="form-group">
 			            {!! Form::label('Date of birth', '', array('for'=>'dateOfBirth')) !!}
-			            {!! Form::text('dateOfBirth', '', array('class'=>'form-control datepicker', 'placeholder'=>'Date of birth', 'id'=>'dateOfBirth')) !!}
+			            {!! Form::text('dateOfBirth', Input::old('dateOfBirth'), array('class'=>'form-control datepicker', 'placeholder'=>'Date of birth', 'id'=>'dateOfBirth')) !!}
 			        </div>
 			        <div class="form-group">
 			            {!! Form::label('Education Background', '', array('for'=>'education')) !!}
-			            {!! Form::text('education', '', array('class'=>'form-control', 'placeholder'=>'Education Background', 'id'=>'education')) !!}
+			            {!! Form::text('education', Input::old('education'), array('class'=>'form-control', 'placeholder'=>'Education Background', 'id'=>'education')) !!}
 			        </div>
 			        <div class="form-group">
 			        	{!! Form::label('Preferred mode of contact', '', array('for'=>'contactMode')) !!}
-			        	{!! Form::select('contactMode', array('phone'=>'Phone','email'=>'Email','none'=>'None'),'phone',array('class'=>'form-select form-control')) !!}
+			        	{!! Form::select('contactMode', array('phone'=>'Phone','email'=>'Email','none'=>'None'), Input::old('contactMode'),array('class'=>'form-select form-control')) !!}
 			        </div>
 			        {!! Form::button('Submit',array('type'=>'submit','class'=>'btn btn-primary')) !!}
 			    {!! Form::close() !!}	
